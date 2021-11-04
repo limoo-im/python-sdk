87da1e226c598de4a838027304f4d05d563bce20
amirsheikh <51942698+amirsheikh@users.noreply.github.com>
2021-11-04 07:38:47 +0330

    Add function for getting list of users
    Add function for getting list of users in specific workspace.

--- limoo/entities/workspaces.py
+++ limoo/entities/workspaces.py
@@ -9,3 +9,7 @@ class Workspaces:
     _MINE = 'user/my_workspaces'
     async def mine(self):
         return await self._driver._execute_api_get(self._MINE)
+    
+    _GET_MEMBERS = 'workspace/items/{}/members'
+    async def members(self, workspace_id):
+        return await self._driver._execute_api_get(self._GET_MEMBERS.format(workspace_id))
