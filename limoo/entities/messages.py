d8081ef926fd22c531c9ae46ea3d676fe29db6b6
amirsheikh <51942698+amirsheikh@users.noreply.github.com>
2021-12-04 10:42:01 +0330

    add send function for sending message with phone number.

--- limoo/entities/messages.py
+++ limoo/entities/messages.py
@@ -16,3 +16,14 @@ class Messages:
         if files:
             body['files'] = files
         return await self._driver._execute_api_post(self._CREATE.format(workspace_id, conversation_id), body=body)
+
+    _SEND = 'workspace/items/{}/message/items'
+    async def send(self, workspace_id, receiver_phone_number, text, *, thread_root_id=None, direct_reply_message_id=None, files=None):
+        body = {'receiver_phone_number': receiver_phone_number, 'text': text}
+        if thread_root_id:
+            body['thread_root_id'] = thread_root_id
+        if direct_reply_message_id:
+            body['direct_reply_message_id'] = direct_reply_message_id
+        if files:
+            body['files'] = files
+        return await self._driver._execute_api_post(self._SEND.format(workspace_id), body=body)
