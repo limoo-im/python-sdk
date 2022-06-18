ff1a0d28447dfc4c55091db52ee3b5fa723fa327
amirsheikh <51942698+amirsheikh@users.noreply.github.com>
2022-06-18 15:20:38 +0430

    Implement functions for archive and unarchive groups

--- limoo/entities/conversations.py
+++ limoo/entities/conversations.py
@@ -6,6 +6,11 @@ class Conversations:
     def __init__(self, driver):
         self._driver = driver
 
+    _GET = 'workspace/items/{}/conversation/items/{}'
+    async def get(self, workspace_id, conversation_id):
+        endpoint = self._GET.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_get(endpoint)
+
     _CREATE = 'workspace/items/{}/conversation/items'
     async def create(self, workspace_id, *, user_ids=[], conversation_type='direct', display_name=None):
         body = {
@@ -43,4 +48,18 @@ class Conversations:
         endpoint = self._ADD.format(workspace_id, conversation_id)
         body = users
         return await self._driver._execute_api_post(endpoint, body=body)
+
+    _ARCHIVE = "workspace/items/{}/conversation/items/{}/archive"
+    async def archive(self, workspace_id, conversation_id):
+        body = {}
+        endpoint = self._ARCHIVE.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_post(endpoint, body=body)
+    
+    _UNARCHIVE = "workspace/items/{}/conversation/items/{}/unarchive"
+    async def unarchive(self, workspace_id, conversation_id):
+        body = {}
+        endpoint = self._UNARCHIVE.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_post(endpoint, body=body)
+
+
 
