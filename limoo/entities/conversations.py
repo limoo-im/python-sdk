92a3737be684dc23c04bae16c710d69e745c0aae
amirsheikh <51942698+amirsheikh@users.noreply.github.com>
2022-06-18 17:55:48 +0430

    implement functions to create a group and add users to it. (#6)
    * implement functions to create a group and add users to it.
    
    * Implement functions for archive and unarchive groups
    
    * change the name of the function to add_members.

--- limoo/entities/conversations.py
+++ limoo/entities/conversations.py
@@ -6,12 +6,19 @@ class Conversations:
     def __init__(self, driver):
         self._driver = driver
 
+    _GET = 'workspace/items/{}/conversation/items/{}'
+    async def get(self, workspace_id, conversation_id):
+        endpoint = self._GET.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_get(endpoint)
+
     _CREATE = 'workspace/items/{}/conversation/items'
-    async def create(self, workspace_id, *, user_ids):
+    async def create(self, workspace_id, *, user_ids=[], conversation_type='direct', display_name=None):
         body = {
             'user_ids': user_ids,
-            'type': 'direct',
+            'type': conversation_type,
         }
+        if display_name:
+            body["display_name"] = display_name
         return await self._driver._execute_api_post(self._CREATE.format(workspace_id), body=body)
 
     _PUBLICS = 'workspace/items/{}/conversation/public'
@@ -35,3 +42,24 @@ class Conversations:
         if params:
             endpoint = f'{endpoint}?{urllib.parse.urlencode(params)}'
         return await self._driver._execute_api_get(endpoint)
+
+    _ADDـMEMBERS = "workspace/items/{}/conversation/items/{}/members/batch"
+    async def add_members(self, workspace_id, conversation_id, users):
+        endpoint = self._ADD.format(workspace_id, conversation_id)
+        body = users
+        return await self._driver._execute_api_post(endpoint, body=body)
+
+    _ARCHIVE = "workspace/items/{}/conversation/items/{}/archive"
+    async def archive(self, workspace_id, conversation_id):
+        body = {}
+        endpoint = self._ARCHIVE.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_post(endpoint, body=body)
+    
+    _UNARCHIVE = "workspace/items/{}/conversation/items/{}/unarchive"
+    async def unarchive(self, workspace_id, conversation_id):
+        body = {}
+        endpoint = self._UNARCHIVE.format(workspace_id, conversation_id)
+        return await self._driver._execute_api_post(endpoint, body=body)
+
+
+
